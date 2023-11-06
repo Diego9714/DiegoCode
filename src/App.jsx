778a08496779854379580a6ba7050aa6b817c01d
Diego9714 <diegoacabreram@hotmail.com>
2023-11-06 02:06:43 -0400

    Links section, about me and finished experience. Starting with contact area...

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
-import Home from './components/Home'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import Experience from './components/Experience'
+import Portfolio from './components/Portfolio'
+import SocialLinks from "./components/SocialLinks";
 
 function App() {
   return (
     <>
       <Navbar/>
       <Home/>
+      <About/>
+      <Experience/>
+      <Portfolio/>
+      <SocialLinks />
     </>
   )
 }
